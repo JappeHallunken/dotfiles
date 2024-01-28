4a35af8e946aaf2a4dbb7f5f2493a7d4562dcc0c
jappe <jappe@posteo.de>
2024-01-28 14:04:16 +0100

    update configs

---
+++ cp_configs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#set path
+dotpath="$HOME/dotfiles"
+cd "$dotpath"
+
+## execute rsync
+/usr/bin/rsync -a $HOME/.config/bashconf/.bashrc.d/.custom "$dotpath/bash" && \
+/usr/bin/rsync -a $HOME/.config/nvim/lua/custom/* "$dotpath/nvim" && \
+/usr/bin/rsync -a $HOME/.config/sway/config "$dotpath/sway" && \
+/usr/bin/rsync -a $HOME/.config/waybar/* "$dotpath/waybar"
+
+## exec git if rsync was succesful
+if [ $? -eq 0 ]; then
+    /usr/bin/git add .
+    /usr/bin/git commit -m "update configs"
+    /usr/bin/git push
+else
+    echo "An error occured, git was not executed!"
+    exit 1
+fi
