62c3206c6f863510b8c6aa577325211fb0644375
jappe <jappe@posteo.de>
2024-01-28 01:00:16 +0100

    without symlink

---
+++ nvim/init.lua
@@ -0,0 +1,12 @@
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+vim.opt.number = true
+vim.opt.relativenumber = true
+--require('custom.live_server')
+--require('custom.configs.live_server')
+
---
+++ nvim/mappings.lua
@@ -0,0 +1,33 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+
+    --  format with conform
+    ["<leader>fm"] = {
+      function()
+        require("conform").format()
+      end,
+      "formatting",
+    }
+
+  },
+  v = {
+    [">"] = { ">gv", "indent"},
+  },
+}
+
+-- more keybinds!
+M.codeium = {
+     i = {
+     ["<C-ä>"] = {function () return vim.fn['codeium#Accept']() end, opts = { expr = true, silent = true }},
+     ["<C-,>"] = {function () return vim.fn['codeium#CycleCompletions'](1) end, opts = { expr = true, silent = true }},
+     ["<C-.>"] = {function () return vim.fn['codeium#CycleCompletions'](-1) end, opts = { expr = true, silent = true }},
+     ["<C-ö>"] = {function () return vim.fn['codeium#Clear']() end, opts = { expr = true, silent = true }},
+     ["<C-l>"] = {function () return vim.fn['codeium#Complete']() end, opts = { expr = true, silent = true }},
+     },
+}
+
+return M
