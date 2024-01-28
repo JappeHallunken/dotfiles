593e63876c23b09fcfa2e8a97d7a38b8b88d013f
jappe <jappe@posteo.de>
2024-01-28 13:37:12 +0100

    ignore .gitignore and the script

--- cp_configs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-pfad="/home/jappe/dotfiles"
-
-rsync -a /home/jappe/.config/bashconf/.bashrc.d/.custom $pfad/bash 
-rsync -a /home/jappe/.config/nvim/lua/custom/* $pfad/nvim 
-rsync -a /home/jappe/.config/sway/config $pfad/sway 
-rsync -a /home/jappe/.config/waybar/* $pfad/waybar
