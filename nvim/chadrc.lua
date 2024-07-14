2aed4e64a2b5c9e7af6bbe86985ea49df7df5788
Paul Nürnberger <jappe@posteo.de>
2024-07-14 11:23:46 +0200

    update configs

--- nvim/chadrc.lua
+++ nvim/chadrc.lua
@@ -6,8 +6,8 @@ local highlights = require "custom.highlights"
 
 
 M.ui = {
-  theme = "github_light",
-  theme_toggle = { "github_light", "github_light" },
+  theme = "ayu_light",
+  theme_toggle = { "ayu_light", "ayu_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
