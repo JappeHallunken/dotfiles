d7eed070ace3a4c64324b5a1dd844151cc51d598
Paul Nürnberger <jappe@posteo.de>
2024-11-25 23:49:14 +0100

    update configs

--- cp_configs.sh
+++ cp_configs.sh
@@ -12,7 +12,12 @@ cd "$dotpath"
 /usr/bin/rsync -a $HOME/.config/rofi/config.rasi "$dotpath/rofi" && \
 /usr/bin/rsync -a $HOME/.swaylock/config "$dotpath/swaylock" && \
 /usr/bin/rsync -a $HOME/.local/bin/waybar-timer.sh  "$dotpath/waybar-timer.sh" && \
-/usr/bin/rsync -a $HOME/.config/foot/foot.ini "$dotpath/foot"
+/usr/bin/rsync -a $HOME/.config/foot/foot.ini "$dotpath/foot/" && \
+/usr/bin/rsync -a $HOME/.config/swaynag/config "$dotpath/swaynag" && \
+/usr/bin/rsync -a $HOME/.config/darkman/config.yaml "$dotpath/darkman" && \
+/usr/bin/rsync -a /usr/share/dark-mode.d/* "$dotpath/darkman/" && \
+/usr/bin/rsync -a /usr/share/light-mode.d/* "$dotpath/darkman/"
+
 
 ## exec git if rsync was succesful
 if [ $? -eq 0 ]; then
