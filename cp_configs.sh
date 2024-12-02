f1062d52ad95e660b4a23b3dd786a416d675699c
Paul Nürnberger <jappe@posteo.de>
2024-12-02 01:36:57 +0100

    update configs

--- cp_configs.sh
+++ cp_configs.sh
@@ -5,18 +5,18 @@ dotpath="$HOME/dotfiles"
 cd "$dotpath"
 
 ## execute rsync
-/usr/bin/rsync -a $HOME/.bashrc.d/custom "$dotpath/bash" && \
-/usr/bin/rsync -a $HOME/.config/nvim/lua/* "$dotpath/nvim" && \
-/usr/bin/rsync -a $HOME/.config/sway/config "$dotpath/sway" && \
-/usr/bin/rsync -a $HOME/.config/waybar/* "$dotpath/waybar" && \
-/usr/bin/rsync -a $HOME/.config/rofi/config.rasi "$dotpath/rofi" && \
-/usr/bin/rsync -a $HOME/.swaylock/config "$dotpath/swaylock" && \
-/usr/bin/rsync -a $HOME/.local/bin/waybar-timer.sh  "$dotpath/waybar-timer.sh" && \
-/usr/bin/rsync -a $HOME/.config/foot/foot.ini "$dotpath/foot/" && \
-/usr/bin/rsync -a $HOME/.config/swaynag/config "$dotpath/swaynag" && \
-/usr/bin/rsync -a $HOME/.config/darkman/config.yaml "$dotpath/darkman" && \
-/usr/bin/rsync -a /usr/share/dark-mode.d/* "$dotpath/darkman/" && \
-/usr/bin/rsync -a /usr/share/light-mode.d/* "$dotpath/darkman/"
+/usr/bin/rsync -a --delete $HOME/.bashrc.d/custom "$dotpath/bash" && \
+/usr/bin/rsync -a --delete $HOME/.config/nvim/lua/* "$dotpath/nvim" && \
+/usr/bin/rsync -a --delete $HOME/.config/sway/config "$dotpath/sway" && \
+/usr/bin/rsync -a --delete $HOME/.config/waybar/* "$dotpath/waybar" && \
+/usr/bin/rsync -a --delete $HOME/.config/rofi/config.rasi "$dotpath/rofi" && \
+/usr/bin/rsync -a --delete $HOME/.swaylock/config "$dotpath/swaylock" && \
+/usr/bin/rsync -a --delete $HOME/.local/bin/waybar-timer.sh  "$dotpath/waybar-timer.sh" && \
+/usr/bin/rsync -a --delete $HOME/.config/foot/foot.ini "$dotpath/foot/" && \
+/usr/bin/rsync -a --delete $HOME/.config/swaynag/config "$dotpath/swaynag" && \
+/usr/bin/rsync -a --delete $HOME/.config/darkman/config.yaml "$dotpath/darkman" && \
+/usr/bin/rsync -a --delete /usr/share/dark-mode.d/* "$dotpath/darkman/" && \
+/usr/bin/rsync -a --delete /usr/share/light-mode.d/* "$dotpath/darkman/"
 
 
 ## exec git if rsync was succesful
