2884d05a5dfc40267d93ca794d6b5774f6c0ff74
Paul Nürnberger <jappe@posteo.de>
2024-12-06 14:23:01 +0100

    update configs

--- nvim/chadrc.lua
+++ nvim/chadrc.lua
@@ -6,8 +6,8 @@
 local M = {}
 
 M.base46 = {
-  theme = "flex-light",
-  theme_toggle = { "flex-light", "github_dark" },
+  theme = "ayu_light",
+  theme_toggle = { "ayu_light", "github_dark" },
   -- hl_override = {
   -- 	Comment = { italic = true },
   -- 	["@comment"] = { italic = true },
